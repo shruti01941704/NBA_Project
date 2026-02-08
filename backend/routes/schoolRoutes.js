e124a7bef3d0799ca8dadceb9c0cd0f2a1e472e0
shruti01941704 <shrutilakade1711@gmail.com>
2026-02-08 14:16:37 +0530

    Remove all files from repository

--- backend/routes/schoolRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getSchoolById } = require('../controllers/schoolController');
-const { protect } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.route('/:id').get(protect, getSchoolById);
-
-module.exports = router;
--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id, schoolId, role) => {
-  return jwt.sign({ id, schoolId, role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE || '24h', // Default to 24 hours, can be overridden by env var
-  });
-};
-
-module.exports = generateToken;
